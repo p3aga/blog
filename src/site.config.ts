0b50887f6424fbebd27525fd5c3324037671f0a5
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-04-22 03:02:22 -0300

    feat: add label for index links

--- src/site.config.ts
+++ src/site.config.ts
@@ -5,10 +5,10 @@ const siteConfig = {
   Author: 'Pedro Henrick Queiroz',
   Year: 2026,
   UserLinks: [
-    { icon: 'linkedin', url: 'https://www.linkedin.com/in/p3aga' },
-    { icon: 'github', url: 'https://github.com/p3aga' },
-    { icon: 'codeberg', url: 'https://codeberg.org/p3aga' },
-    { icon: 'maildotru', url: 'mailto:pedrohenrickqa@gmail.com' },
+    { icon: 'linkedin', url: 'https://www.linkedin.com/in/p3aga', alt: 'LinkedIn' },
+    { icon: 'github', url: 'https://github.com/p3aga', alt: 'GitHub' },
+    { icon: 'codeberg', url: 'https://codeberg.org/p3aga', alt: 'Codeberg' },
+    { icon: 'maildotru', url: 'mailto:pedrohenrickqa@gmail.com', alt: 'Email' },
   ],
   menuLinks: [
     { path: 'blog', title: 'Blog' },
