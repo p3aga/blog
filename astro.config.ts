b40da87e8841e6dad39d537710441e40860cfa0b
Pedro Henrick <pedrohenrickqa@gmail.com>
2026-06-21 03:20:35 -0300

    Revert "Add Cloudflare Workers configuration"
    This reverts commit 626ab02a218ddca7c28b8853259d17481f050db2.

--- astro.config.ts
+++ astro.config.ts
@@ -10,14 +10,11 @@ import remarkDirective from 'remark-directive';
 import rehypeTableProcessor from './src/plugins/rehype-table-processor';
 import remarkToc from './src/plugins/remark-toc';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
   site: 'https://p3aga.is-a.dev',
   base: '/',
   trailingSlash: 'ignore',
-
   integrations: [
     astroExpressiveCode({
       themes: ['gruvbox-dark-medium'],
@@ -33,19 +30,14 @@ export default defineConfig({
     icon(),
     sitemap(),
   ],
-
   markdown: {
     remarkPlugins: [remarkDirective, remarkToc],
     rehypePlugins: [rehypeTableProcessor],
   },
-
   vite: {
     plugins: [tailwindcss()],
   },
-
   devToolbar: {
     enabled: false,
   },
-
-  adapter: cloudflare(),
 }) as AstroUserConfig;
